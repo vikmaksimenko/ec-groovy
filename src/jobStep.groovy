5495bfa03ba09d72fbf8d1df56a292b8c3805e12
Viktor Maksymenko <vikmaksimenko@hotmail.com>
2018-03-06 19:12:52 +0200

    Updated createJobStep test with validations

--- src/jobStep.groovy
+++ src/jobStep.groovy
@@ -5,10 +5,15 @@ import com.electriccloud.client.groovy.ElectricFlow
 ElectricFlow ef = new ElectricFlow()
 ef.login('192.168.4.133', 'admin', 'changeme')
 
-def result = ef.createJobStep(jobStepName: 'jobStep', command: 'echo hello', { response, data ->
+ef.createJobStep(jobStepName: 'jobStep', command: 'echo hello', { response, data ->
     println 'Job step created'
-    println data.dump
+    println data.dump()
+
+    assert data.jobStep.outcome     == 'success'
+    assert data.jobStep.stepName    == 'jobStep'
+    assert data.jobStep.command     == 'echo hello'
+
 }, { response, data ->
     println 'Failed to create job step'
-    println data.dump
+    println data.dump()
 })
